4d657d4038c7a485074463e31d5f37245b6f4772
wkoks <ivan.ohrimenko@hotmail.com>
2014-12-14 05:14:12 +0300

    Initialize Col-Collage and Finish registr and authenfication by omni+omni_facebook. PS with some bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ColCollage::Application.config.secret_key_base = '8b6e38754b63c2ab2859155b3eac430d509ccc74bb4a9b5d4604dd7eb734e438ed56765e0bfa95c03247d23792fd5afb4207cd126b3f44a8cff013ed5cd4e5d6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ColCollage::Application.config.session_store :cookie_store, key: '_Col_Collage_session'
